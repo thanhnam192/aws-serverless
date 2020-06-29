69abe16a75c61752832d326e752953aa5616f07c
Nguyen Thanh Nam <thanhnam192@gmail.com>
2020-06-29 23:46:07 +0700

    sample
    sample

---
+++ greetMe/index.js
@@ -0,0 +1,27 @@
+const moment = require('moment');
+
+const greetings = {
+    'en': 'hello',
+    'vn': 'xin chao',
+    'fr': 'Franco'
+};
+
+
+exports.handler = async (event) => {
+    const name = event.pathParameters.name;
+    let {lang,...info} = event.queryStringParameters;
+
+    const greeting = greetings[lang] ? greetings[lang] : greetings['en'];
+    const message = `${greeting} ${name}`;
+    const response = {
+        message,
+        info,
+        timetamps: moment().unix(1318781876)
+    };
+
+    return {
+        'statusCode' : 200,
+        'body': JSON.stringify(response)
+    };
+
+}
